2234f4a471c806ffdb5e45b1f2a04fed34fce2c2
The6LAMB <147244656+The6LAMB@users.noreply.github.com>
2024-08-15 10:47:58 +0200

    new fucking start

---
+++ Mele2D/Assets/PlayerSpawner.cs
@@ -0,0 +1,15 @@
+using Fusion;
+using UnityEngine;
+
+public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
+{
+    public GameObject PlayerPrefab;
+    
+    public void PlayerJoined(PlayerRef player)
+    {
+        if (Runner.LocalPlayer == player)
+        {
+            Runner.Spawn(PlayerPrefab, new Vector3(0,1,-5), Quaternion.identity);
+        }
+    }
+}
